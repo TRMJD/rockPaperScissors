6790ed73505eb535890c3a61a21762bd2ceb452f
TRMJD <joshua.defiesta@yahoo.com>
2023-08-07 16:20:20 -0700

    completed draft

---
+++ java.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+//Objective: Create a Rock Paper Scissors game.
+//Rock Paper Scissors is a game of 2 players, in this case a 
+//user input player against a computer. The players must choose 
+//either rock, paper, or scissors. If rock goes against scissors, 
+//rock wins. If paper goes against rock, paper wins. If scissors 
+//goes against paper, scissors wins. 
+
+//Algorithm
+//display a message, prompting a user to input rock, paper, or scissors
+//store the user's input in a variable
+//create a variable for the computer's input (random input)
+//compare the variables with each scenario to see who wins
+//display the winner
+
