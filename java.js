d64e231e4a228405e4be6a4e17432e0257f6c5fc
TRMJD <joshua.defiesta@yahoo.com>
2024-01-06 18:28:45 -0800

    v1 completed

--- java.js
+++ java.js
@@ -1,5 +1,70 @@
 console.log("Hello World!");
 
+
+//DOM 
+const container = document.querySelector('.container').style;
+const playerButtons = document.querySelector('#buttons');
+//create player buttons
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.textContent = "ROCK";
+paper.textContent = "PAPER";
+scissors.textContent = "SCISSORS";
+
+
+
+//CSS style rules (scoreboard)
+const scoreboard = document.querySelector('#scoreboard');
+//title
+const title = document.getElementById('header');
+title.style.textAlign = "center";
+//display the winner
+const scoreText = document.getElementById('winner');
+scoreText.textContent = "";
+scoreText.style.textAlign = "center";
+scoreText.textContent = "";
+//instructions
+const instructions = document.getElementById('roundCount');
+instructions.textContent = "Play a classic game of Rock Paper Scissors against a Computer. Best of 5. Choose your weapon:";
+instructions.style.textAlign = 'center';
+//count of score
+const scoreCount = document.getElementById('scoreCount');
+scoreCount.style.textAlign = 'center';
+//display both player's selections
+const playerSelection = document.getElementById('playerSelection');
+const computerSelection = document.getElementById('computerSelection');
+playerSelection.style.textAlign = 'center';
+computerSelection.style.textAlign = 'center';
+
+scoreboard.appendChild(scoreText);
+scoreboard.appendChild(instructions);
+scoreboard.appendChild(scoreCount);
+
+
+
+
+
+
+//Container styles
+container.height = '100vh';
+container.width = 'auto';
+container.display = 'flex';
+container.flexDirection = 'column';
+container.justifyContent = 'center';
+container.alignItems = 'center';
+
+//scoreboard styles
+scoreboard.style.maxWidth = '350px';
+
+
+
+
+
+
+
+
 //Objective: Create a Rock Paper Scissors game.
 //Rock Paper Scissors is a game of 2 players, in this case a 
 //user input player against a computer. The players must choose 
@@ -10,8 +75,11 @@ console.log("Hello World!");
 //Algorithm
 //display a message, prompting a user to input rock, paper, or scissors
 //store the user's input in a variable
-let userInput;
- 
+let userInput = '';
+let round = 5;
+let playerScore = 0;
+let computerScore = 0;
+let roundwinner;
 
 
 //create a variable for the computer's input (random input)
@@ -38,71 +106,110 @@ function getComputerChoice() {
 //compare the variables with each scenario to see who wins
 function playRound(userInput, computerInput){
     let winner;
-    userInput = prompt('Rock Paper Scissors!\nChoose one: ');
+    //userInput = prompt('Rock Paper Scissors!\nChoose one: ');
     computerInput = getComputerChoice();
     if (userInput.toLowerCase() == 'rock' && computerInput == 'Rock'){
-        alert('Game is tied.');
+        scoreText.textContent = "Tie!";
     }
     else if (userInput.toLowerCase() == 'rock' && computerInput == 'Paper'){
-        alert('Computer Wins.');
+        scoreText.textContent = "Computer Wins!";
         winner = 'computer';
         return winner;
     }
     else if (userInput.toLowerCase() == 'rock' && computerInput == 'Scissors'){
-        alert('Congrats. You Won!');
+        scoreText.textContent = "You Win!";
         winner = 'player';
         return winner;
     }
     else if (userInput.toLowerCase() == 'paper' && computerInput == 'Paper'){
-        alert('Game is tied.');
+        scoreText.textContent = "Tie!";
     }
     else if (userInput.toLowerCase() == 'paper' && computerInput == 'Rock'){
-        alert('Congrats. You Won!');
+        scoreText.textContent = "You Win!";
         winner = 'player';
         return winner;
     }
     else if (userInput.toLowerCase() == 'paper' && computerInput == 'Scissors'){
-        alert('L. The Computer Won.');
+        scoreText.textContent = "Computer Wins!";
         winner = 'computer';
         return winner;
     }
     else if (userInput.toLowerCase() == 'scissors' && computerInput == 'Scissors'){
-        alert('Game is tied.');
+        scoreText.textContent = "Tie!";
     }
     else if (userInput.toLowerCase() == 'scissors' && computerInput == 'Paper'){
-        alert('Congrats. You Won!');
+        scoreText.textContent = "You Win!";
         winner = 'player';
         return winner;
     }
     else if (userInput.toLowerCase() == 'scissors' && computerInput == 'Rock'){
-        alert('You lost.');
+        scoreText.textContent = "Computer Wins!";
         winner = 'computer';
         return winner;
     }
     else {
-        alert('not a selection.')
+        console.log('not a selection.')
     }
 
     
 }
 
+
+
+
+
+rock.onclick = () => {
+    if (round >= 0) {
+        roundwinner = playRound(userInput = 'rock');
+        game();
+       }
+    else checkWinner();
+    
+};
+paper.onclick = () => {
+    if (round >= 0) {
+        roundwinner = playRound(userInput = 'paper');
+        game();
+       }
+    else checkWinner();
+};
+scissors.onclick = () => {
+   if (round >= 0) {
+    roundwinner = playRound(userInput = 'scissors');
+    game();
+   }
+   else checkWinner();
+    
+};
 //create a function that keeps a 5 round game. 
-function game(){
-    let round = 5;
-    let score = 0;
-    let roundwinner;
-    console.log('test');
-    for (round; round > 0; round--){
-        roundwinner = playRound(userInput, computerInput);
-        if (roundwinner == 'player'){
-            score += 1;
-        }
-        else if (roundwinner == 'computer'){
-            score -= 1;
-        }
-        console.log(roundwinner);
-        console.log(score);
-        console.log(round);
+function game() {
+    
+    if (roundwinner == 'player'){
+        playerScore += 1;
     }
-}
-game();
+    else if (roundwinner == 'computer'){
+        computerScore += 1;
+    }
+    console.log("winner: " + roundwinner);
+    console.log("round: " + round);
+    console.log("your score: " + playerScore);
+    console.log("computer's score: " + computerScore);
+    console.log("player chose " + userInput);
+
+    //display the results
+    scoreCount.textContent = "Your Score: " + playerScore;
+    instructions.textContent = "Round: " + round;
+    playerSelection.textContent = "You Chose: " + userInput.toUpperCase();
+    computerSelection.textContent = "Computer Chose: " + computerInput.toUpperCase();
+    round--;
+ }
+
+ function checkWinner() {
+    if (playerScore > computerScore) {
+        scoreText.textContent = "Game Over! The winner is YOU with the score of: " + playerScore + " out of 5 rounds.";
+    }
+    else if (playerScore < computerScore) {
+        scoreText.textContent = "Game Over! The winner is COMPUTER with the score of: " + computerScore + " out of 5 rounds.";
+    }
+    else scoreText.textContent = "Game Over! The Game is TIED!";
+ }
